cba7ae7c75e09abcbf3a509a005124df73e99b6c
Caio Corrêa <searchoncaiozinho@gmail.com>
2024-05-23 10:49:52 -0300

    api finalizada

--- backend/aula10/routes/router_produtos.js
+++ backend/aula10/routes/router_produtos.js
@@ -10,6 +10,11 @@ router.get("/", controllerProdutos.listar);
 
 router.get("/:id", controllerProdutos.buscar, controllerProdutos.obter);
 
-router.put('/:id', controllerProdutos.atualizar)
+router.put("/:id", 
+controllerProdutos.buscar,
+controllerProdutos.validar,  
+controllerProdutos.atualizar);
+
+router.delete('/:id', controllerProdutos.buscar, controllerProdutos.remover);
 
 module.exports = router;
--- backend/aula10/tests/router_produtos.test.js
+++ backend/aula10/tests/router_produtos.test.js
@@ -7,13 +7,13 @@ const req = supertest(app);
 let id = null;
 
 describe("API Loja Virtual - Produtos", () => {
-  // test("Deve retornar 201 e um JSON no POST /produtos", async () => {
-  //   const res = await req
-  //     .post("/produtos")
-  //     .send({ nome: "Morango", preco: 9.0 });
-  //   expect(res.status).toBe(201);
-  //   expect(res.type).toBe("application/json");
-  // });
+  test("Deve retornar 201 e um JSON no POST /produtos", async () => {
+    const res = await req
+      .post("/produtos")
+      .send({ nome: "Morango", preco: 9.0 });
+    expect(res.status).toBe(201);
+    expect(res.type).toBe("application/json");
+  });
 
   test("Deve retorna 422 e JSON no POST /produtos", async () => {
     const res = await req.post("/produtos").send({});
@@ -31,21 +31,46 @@ describe("API Loja Virtual - Produtos", () => {
   });
 
   test("Deve retornar 200 e JSON no GET /produtos/id", async () => {
-    const res = await req.get(`/produtos/${id}`)
+    const res = await req.get(`/produtos/${id}`);
     expect(res.status).toBe(200);
     expect(res.type).toBe("application/json");
   });
 
   test("Deve retornar 404 e JSON no GET /produtos/id", async () => {
-    const res = await req.get('/produtos/663e07803dafbc71e0e86c8c')
+    const res = await req.get("/produtos/663e07803dafbc71e0e86c8c");
     expect(res.status).toBe(404);
     expect(res.type).toBe("application/json");
-
   });
 
   test("Deve retornar 200 e JSON no PUT /prdutos/id", async () => {
-    const res = await req.put(`/produtos/${id}`).send ({nome: "Morango Azul", preco: 8.9})
+    const res = await req
+      .put(`/produtos/${id}`)
+      .send({ nome: "Morango Azul", preco: 8.9 });
     expect(res.status).toBe(200);
+    expect(res.type).toBe("application/json");
+  });
+
+  test("Deve retornar 404 e JSON no PUT /produtos/id", async () => {
+    const res = await req.put("/produtos/663e07803dafbc71e0e86c8c");
+    expect(res.status).toBe(404);
+    expect(res.type).toBe("application/json");
+  });
+
+  test("Deve retornar 422 e JSON no PUT /prdutos/id", async () => {
+    const res = await req.put(`/produtos/${id}`);
+    expect(res.status).toBe(422);
+    expect(res.type).toBe("application/json");
+  });
+
+  test("Deve retornar 204 e JSON no DELETE /prdutos/id", async () => {
+    const res = await req.delete(`/produtos/${id}`);
+    expect(res.status).toBe(204);
+    expect(res.type).toBe("");
+  });
+
+  test("Deve retornar 404 e JSON no DELETE /prdutos/id", async () => {
+    const res = await req.delete(`/produtos/${id}`);
+    expect(res.status).toBe(404);
     expect(res.type).toBe("application/json");
   });
 });
